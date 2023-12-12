e0af6558612da95f719a48c0363b37492b1044b2
ggit14103 <giang2k3qtkx@gmail.com>
2023-12-12 23:32:09 +0700

    up files

---
+++ _Game/_Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
